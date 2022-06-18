8d7ff7003115eb5885a8990bc5c342a3ab29a521
jaguilar97 <98626422+jaguilar97@users.noreply.github.com>
2022-06-17 20:34:05 -0600

    Semana 5

--- src/main/java/com/Tienda/controller/IndexController.java
+++ src/main/java/com/Tienda/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.Tienda.controller;
 
 import com.Tienda.dao.ClienteDao;
 import com.Tienda.domain.Cliente;
+import com.Tienda.service.ClienteService;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class IndexController {
     
     @Autowired
-    private ClienteDao clienteDao;
+    private ClienteService clienteService;
     
     @GetMapping("/")
     public String inicio(Model model){
@@ -31,10 +32,35 @@ public class IndexController {
      var clientes = Arrays.asList(cliente, cliente2, cliente3);
      //var clientes = Arrays.asList();*/
      
-     var clientes = clienteDao.findAll();
+     var clientes = clienteService.getClientes();
      
      model.addAttribute("clientes", clientes);
      
      return "index";
     }
+    
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente){
+        return "modificarCliente";
+    }
+    
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente){
+        clienteService.save(cliente);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/modificarCliente/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model){
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificarCliente";
+    }
+    
+    @GetMapping("/eliminarCliente/{idCliente}")
+    public String eliminarCliente(Cliente cliente){
+        clienteService.delete(cliente);
+        return "redirect:/";
+    }
+    
 }
