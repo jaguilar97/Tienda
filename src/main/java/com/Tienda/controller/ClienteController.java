1f5227e8168925ef667a62a1f5ebd45449f586fe
jaguilar97 <98626422+jaguilar97@users.noreply.github.com>
2022-08-10 11:47:06 -0600

    ReporteCliente

--- src/main/java/com/Tienda/controller/ClienteController.java
+++ src/main/java/com/Tienda/controller/ClienteController.java
@@ -1,8 +1,14 @@
 package com.Tienda.controller;
 
 import com.Tienda.domain.Cliente;
+import com.Tienda.service.ClienteReportService;
 import com.Tienda.service.ClienteService;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +22,10 @@ public class ClienteController {
     
     @Autowired
     private ClienteService clienteService;
+    
+    @Autowired
+    private ClienteReportService clienteReportService;
+
     
     @GetMapping("/cliente/listado")
     public String inicio(Model model) {
@@ -54,6 +64,24 @@ public class ClienteController {
     public String eliminarCliente(Cliente cliente){
         clienteService.delete(cliente);
         return "redirect:/cliente/listado";
+    }
+    
+    @GetMapping(value = "/cliente/ReporteClientes", produces = MediaType.APPLICATION_PDF_VALUE)
+    public @ResponseBody
+    byte[] getFile() throws IOException {
+        try {
+            FileInputStream fis = new FileInputStream(new File(clienteReportService.generateReport()));
+            byte[] targetArray = new byte[fis.available()];
+            fis.read(targetArray);
+            return targetArray;
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
     
 }
